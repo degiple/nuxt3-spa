f24d05ffdba4ee5474b03581059bc7248024c57b
Konosuke Takeshima <k.takeshima@degiple.com>
2022-12-10 15:15:19 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLayout :name="layout">
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+const admin = true;
+const layout = admin ? 'default': 'custom';
+</script>
